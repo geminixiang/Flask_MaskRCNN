e0211755f899e9ac1f907a098bce6a7f90fc0c58
xiang-server <f416720001@yahoo.com>
2022-02-03 11:24:43 +0800

    new model of 27 class

--- app.py
+++ app.py
@@ -28,8 +28,9 @@ app = Flask(__name__, static_folder='static')
 app.config['JSON_AS_ASCII'] = False
 app.config['SECRET_KEY'] = os.urandom(24)
 # class_names = ['BG', '人', '房子', '樹', '時鐘', '人力車', '轎子', '船', '貓', '羊', '牛', '象', '斑馬', '傘', '領帶', '扇', '水瓶', '碗', '椅', '盆栽植物', '桌', '水桶', '書', '花瓶', '馬匹', '炮', '火車', '杯', '畫作', '燈籠', '路燈', '槍', '氣球', '馬車', '十字架', '橋', '刀具', '狗', '蠟燭', '鏢', '孩童', '軍隊', '電線桿', '望遠鏡', '雕像', '鼓', '軍旗', '城牆', '旗幟', '城門', '蒸汽機', '儀表', '畫像', '賽馬場', '馬廄', '琵琶', '匾額', '掛燈', '珊瑚', '船帆', '門', '守望台', '藤牌', '魚', '水稻', '亭子', '煙囪', '鐵路', '帳篷', '煤', '十字鎬', '眼鏡', '水龍', '棺材', '官員', '電報局', '英皇子', '日本國旗', '美國國旗', '西醫', '法國國旗', '絞刑', '水雷', '西妓', '教堂', '木馬', '窗', '壁燈', '大鐘', '花窗玻璃', '英國國旗', '船錨', '水煙斗', '天文台', '砲彈', '雞', '猴子', '香爐', '牌位', '軍官', '軍人', '女性', '潛水艇', '消防員', '屍體', '罪犯', '警官', '庸醫', '曾國藩', '當舖', '鳥籠', '濕版攝影相機', '井', '大水缸', '報紙', '公主', '鞭子', '戎克船/䑸', '龍舟', '長喇叭', '鑼', '玉皇宮']
-class_names = ['BG', '人', '房子', '樹', '船', '扇', '椅', '桌', '孩童', '門', '窗', '軍人', '女性', '帽', '圖像標題', '圖像文字', '印章文字']
-model_file = "./models/500x20_all.h5"
+# class_names = ['BG', '人', '房子', '樹', '船', '扇', '椅', '桌', '孩童', '門', '窗', '軍人', '女性', '帽', '圖像標題', '圖像文字', '印章文字']
+class_names = ['BG', '人', '房子', '樹', '船', '扇', '椅', '盆栽植物', '桌', '杯', '燈籠', '槍', '刀具', '孩童', '旗幟', '門', '窗', '軍人', '女性', '帽', '官兵', '官員', '官役', '蠟燭', '蛤蠣', '圖像標題', '圖像文字', '印章文字']
+model_file = "./models/27cls_500x30_all_20.h5"
 try:
     import time, stat
     model_modify_time = time.ctime(os.stat(model_file)[stat.ST_MTIME])
@@ -40,10 +41,10 @@ class InferenceConfig(Config):
     NAME = "dian"
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
-    NUM_CLASSES = 1 + 16
+    NUM_CLASSES = 1 + 27
     IMAGE_MIN_DIM = 1024
     IMAGE_MAX_DIM = 1024
-    DETECTION_MIN_CONFIDENCE = 0.7
+    DETECTION_MIN_CONFIDENCE = 0.8
 
 graph = tf.get_default_graph()
 model = modellib.MaskRCNN( mode="inference", 
@@ -274,7 +275,7 @@ def MaskRCNN():
     image.save(file_name_origin)
     save_image(image_array, file_name_mask, r['rois'], r['masks'],
             r['class_ids'], r['scores'], class_names,
-            scores_thresh=0.5, mode=0)
+            scores_thresh=0.8, mode=0)
 
     return make_response(resp)
 
